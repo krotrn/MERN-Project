6b589efdbbeec4be894c5c63d61eb17f094fb7ae
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-12-28 00:26:37 +0530

    Update cookie settings for JWT token to support cross-origin requests

--- server/utils/createToken.js
+++ server/utils/createToken.js
@@ -1,18 +1,17 @@
 import jwt from "jsonwebtoken";
 
-// Generate JWT token and set it as an HTTP-only cookie
 const generateToken = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+    expiresIn: "30d", // Token expiration time
   });
 
-  // set JWT token as an HTTP-only cookie
   res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // Prevent client-side JavaScript access
+    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+    sameSite: "none", // Required for cross-origin requests
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
+
   return token;
 };
 
