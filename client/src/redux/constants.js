5a9f1a0fcd363c88bada0ec1edd586b9c727c5a6
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-12-27 20:55:43 +0530

    Refactor API URL constants to use BASE_URL for consistency

--- client/src/redux/constants.js
+++ client/src/redux/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = `https://mern-project-k8u0.onrender.com`;
-export const USER_URL = `/api/v1/users`;
-export const GENRE_URL = `/api/v1/genres`;
-export const MOVIE_URL = `/api/v1/movies`;
-export const UPLOAD_URL = `/api/v1/uploads`;
+export const USER_URL = `${BASE_URL}/api/v1/users`;
+export const GENRE_URL = `${BASE_URL}/api/v1/genres`;
+export const MOVIE_URL = `${BASE_URL}/api/v1/movies`;
+export const UPLOAD_URL = `${BASE_URL}/api/v1/uploads`;
