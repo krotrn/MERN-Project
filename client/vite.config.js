af40c4853dc17a36499c80d7b7958dfd72167bc6
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-12-27 20:34:18 +0530

    Update Vite config to set production API and uploads targets

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     server: {
         proxy: {
             "/api/": {
-                target: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+                target: "https://mern-project-k8u0.onrender.com",
                 changeOrigin: true,
                 secure: false,
             },
             "/uploads/": {
-                target: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+                target: "https://mern-project-k8u0.onrender.com",
                 changeOrigin: true,
                 secure: false,
             },
