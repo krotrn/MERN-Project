32bf0331318e9d2025f7bde27fb3f82d62a2ef60
Karan Kumar Sah <karankumarsahtkg2018@gmail.com>
2024-12-28 18:21:42 +0530

    Add credentials option to base query for API requests

--- client/src/redux/api/apiSlice.js
+++ client/src/redux/api/apiSlice.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL, credentials: "include" });
 
 export const apiSlice = createApi({
     baseQuery,
